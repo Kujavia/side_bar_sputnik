e54778e4796519fcfe8c98c6a07d02a5534808ee
SputnikPro <averina@sputnik.llc>
2024-11-18 10:12:18 +0300

    fm

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import {resolve} from "path";
+
+export default defineConfig({
+  base: '/side_bar_sputnik/', 
+  build: {
+		outDir: 'dist',
+		// rollupOptions: {
+		// 	input: {
+		// 		index: resolve(__dirname, "index.html"),
+		// 		action: resolve(__dirname, "src/html/action.html"),
+		// 		parameters: resolve(__dirname, "src/html/parameters.html"),
+		// 	}
+		// }
+	}
+});
